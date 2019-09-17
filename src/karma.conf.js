58b104d20be5dec960135622a6e867d3eb093c46
Hallyson Almeida <halmeida@medplan.com.br>
2019-09-17 10:22:45 -0300

    fix: karma chrome testing on travis

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,7 +26,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeNoSandbox'],
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+    },
     singleRun: true
   });
 };
